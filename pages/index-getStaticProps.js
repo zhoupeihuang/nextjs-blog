e30a7227a02b3d004a06f7347c37d34453857a3b
zhoupeihuang <zhoupeihuang152@sina.com>
2023-07-03 15:59:47 +0800

    '修复bug'

--- pages/index-getStaticProps.js
+++ pages/index-getStaticProps.js
@@ -6,7 +6,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import { getSortedPostsData } from '../lib/post';
+import { getSortedPostsData } from '../lib/posts';
 
 
 export default function Home({allPostsData}) {
